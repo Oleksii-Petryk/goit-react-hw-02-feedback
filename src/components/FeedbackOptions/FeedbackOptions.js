8af473bbc5b97fb115a72aa2e977f4d9ef17940e
Oleksii <Alex13867777@gmail.com>
2021-10-23 16:16:27 +0300

    ready for peer review

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import styles from './FeedbackOptions.module.css';
 import PropTypes from 'prop-types';
 
 function FeedbackOptions({ options, onLeaveFeedback }) {
   return (
     <div className={styles.buttons}>
-      {options.map((option, index) => (
+      {options.map(option => (
         <button
           type="button"
           className={styles.button}
-          key={index}
+          key={uuidv4()}
           onClick={() => onLeaveFeedback(option)}
         >
           {option}
